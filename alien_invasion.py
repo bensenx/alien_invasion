a644fb8e916f7ff4379d541b748310509c167133
Bensen <xiebenshuai@gmail.com>
2021-06-09 13:33:28 -0700

    Moving Left and Right

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,7 +17,7 @@ class AlienInvasion:
         """开始游戏的主循环"""
         while True:
             self._check_events()
-            
+            self.ship.update()
             #每次循环时都重绘屏幕。
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
@@ -28,6 +28,17 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        #向右移动飞船
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
 if __name__ == '__main__':
     #创建游戏实例并运行游戏。
     ai = AlienInvasion()
