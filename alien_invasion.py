b4ae2fd94203d2531bbabdf354af838dcf865cd1
Bensen <xiebenshuai@gmail.com>
2021-06-29 16:44:04 -0700

    Fire and bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self) -> None:
@@ -14,6 +15,7 @@ class AlienInvasion:
         #self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.ship =Ship(self)
+        self.bullets = pygame.sprite.Group()
         #设置背景色。
         self.bg_color = (230,230,230)
     def run_game(self):
@@ -21,6 +23,13 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+            #删除消失的子弹
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            #print(len(self.bullets))
+            self._update_screen()
             #每次循环时都重绘屏幕。
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
@@ -44,7 +53,9 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
-            sys.ext()
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self,event):
         """响应松开"""
@@ -52,6 +63,17 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入编组bullets中"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕。"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #创建游戏实例并运行游戏。
