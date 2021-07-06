4cae6599617b55c6911daa0817f3aa5d990dc4c4
Bensenx <xiebenshuai@gmail.com>
2021-07-06 16:03:47 -0700

    adjust the initial speed

--- settings.py
+++ settings.py
@@ -17,16 +17,16 @@ class Settings:
         self.bullets_allowed = 3
         #外星人设置
         #self.alien_speed = 0.5
-        self.fleet_drop_speed =10
+        self.fleet_drop_speed = 5
         #加快游戏节奏的速度
         self.speedup_scale = 1.1
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
         """初始化随游戏进行而变化的设置"""
-        self.ship_speed = 1.5
+        self.ship_speed = 0.5
         self.bullet_speed = 3.0
-        self.alien_speed  = 1.0 
+        self.alien_speed  = 0.3 
         # fleet_direction 为1表示向右，为-1表示向左
         self.fleet_direction = 1
 
